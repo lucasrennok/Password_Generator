15304cce26e5b6a8e62ed660089008de574ad200
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-07-26 21:45:24 -0300

    Buttons okay

--- teste.js
+++ teste.js
@@ -20,30 +20,38 @@ function configClicked(){
         element.style.display = "none";
 }
 function moreClicked(){
-    element = document.getElementById("list_pass");
+    let element = document.getElementById("list_pass");
     let new_id = list_of_pass.length;
-    element.innerHTML = "<il id=\""+new_id+"\"></il></br>"+element.innerHTML;
+    element.innerHTML = "<il id=\""+new_id+"\">Press Generate</il>"+element.innerHTML;
     list_of_pass[new_id]="";
 }
 function generateClicked(){
     let id = list_of_pass.length-1;
-    element = document.getElementById(id.toString());
-    let password = "dasdas";
+    let element = document.getElementById(id.toString());
+    let password = list_of_pass.length.toString();
     /* GERAR ALEATORIAMENTE A SENHA PELOS PARAMETROS DO CONFIG */
     element.innerHTML = "<il id=\""+id+"\">"+password+"</il>";
     list_of_pass[id] = password;
-    console.log(list_of_pass);
 }
 function deleteClicked(){
     if(document.getElementById("all_checkbox").checked){
         //clean all
-        element = document.getElementById("list_pass")
-        element.innerHTML = "<il id=\"0\"></il></br>"
-        list_of_pass = [""]
+        let element = document.getElementById("list_pass");
+        element.innerHTML = "<il id=\"0\">Press Generate</il>";
+        list_of_pass = [""];
         return;
     }
     let id = list_of_pass.length-1;
-    element = document.getElementById(id.toString());
+    if(list_of_pass.length==1){
+        let element = document.getElementById("list_pass");
+        element.innerHTML = "<il id=\"0\">Press Generate</il>";
+        list_of_pass = [""];
+    }else{
+        let element = document.getElementById(id.toString());
+        element.innerHTML =  "";
+        list_of_pass.pop();
+    }
 }
 function downloadClicked(){
+    console.log(list_of_pass);
 }
